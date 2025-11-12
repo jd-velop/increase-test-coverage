80fd4d8277b032181c6587db7fb75636dfb8ee17
Jackson J.D Otis <143106997+jd-velop@users.noreply.github.com>
2025-11-12 19:39:59 +0000

    100% test coverage

--- AtmServices.Test/AtmTests.cs
+++ AtmServices.Test/AtmTests.cs
@@ -11,25 +11,34 @@ public class AtmTests
     int initialBalance = 100;
 
     public AtmTests() {
-
         testAtm = new Atm(initialBalance);
-
     }
 
- 
 
     [Fact]
-
     public void Test_Withdraw()
 
     {
-
-        var result = testAtm.withdraw(25);
-
-        Assert.True(result);
-
+        var withdrawSuccess = testAtm.withdraw(25);
+        Assert.True(withdrawSuccess);
         Assert.Equal(75, testAtm.getBalance());
 
+        var withdrawFails = testAtm.withdraw(150);
+        Assert.False(withdrawFails);
+        Assert.Equal(75, testAtm.getBalance());
+    }
+
+    [Fact]
+    public void Test_Deposit()
+
+    {
+        var depositSuccess = testAtm.deposit(25);
+        Assert.True(depositSuccess);
+        Assert.Equal(125, testAtm.getBalance());
+
+        var depositFails = testAtm.deposit(-25);
+        Assert.False(depositFails);
+        Assert.Equal(125, testAtm.getBalance());
     }
 
 }
